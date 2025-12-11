8ddeec786d7bc09668a2f36fb1b55ea1fd1ca9a1
Abdus-Salam-814 <abdussalam6111997@gmail.com>
2025-12-11 22:55:20 +0600

    clear

--- Intruduction-Javascript-17-13/introduction.js
+++ Intruduction-Javascript-17-13/introduction.js
@@ -11,3 +11,19 @@ const fullName = firstName + ' ' + lastName;
 // console.log(fullName)
 
 
+// loop 
+
+let i = 0;
+while(i < 10){
+    i++;
+
+    console.log(i);
+
+
+}
+
+
+// for loop
+for(let i = 0; i <10; i++){
+    console.log(i)
+}
